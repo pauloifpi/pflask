d3b3417a094a7247acf83bb71fb59719d6c926eb
pauloifpi <paulorodrigo1098@gmail.com>
2025-10-26 16:43:11 -0300

    vercel 2

--- dao/db_config.py
+++ dao/db_config.py
@@ -1,8 +1,9 @@
-import os
 import sqlite3
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, '..', 'banco_escola.db')
+# path / url de conexão
+DB_PATH = "banco_escola.db"
 
 def get_connection():
-    return sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(DB_PATH)
+    return conn
+    
